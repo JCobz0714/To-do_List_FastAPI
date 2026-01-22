e54cf44c34538e11b0df5dba44360978af1c0084
J. cob <jacoboosorio299589@gmail.com>
2026-01-22 18:37:18 -0500

    Creating post endpoint for creating a task, currently working as expected and saving the data into PostgreSQL

--- todo_app/app/routers/tasks.py
+++ todo_app/app/routers/tasks.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlmodel import Session, select
 from app.database import get_session
 from app.models import Task
+from schemas.task import TaskCreate
 
 router = APIRouter(prefix="/tasks", tags=["Tasks"])
 
@@ -10,3 +11,13 @@ async def get_tasks(session: Session = Depends(get_session)):
     statement = select(Task)
     tasks = session.exec(statement).all()
     return tasks
+
+@router.post("/create/", status_code=201)
+async def create_tasks(task_data: TaskCreate, session: Session = Depends(get_session)):
+    db_task = Task(**task_data.model_dump())
+
+    session.add(db_task)
+    session.commit()
+    session.refresh(db_task)
+
+    return db_task
--- todo_app/app/schemas.py
+++ todo_app/app/schemas.py
@@ -1,5 +1,6 @@
 from sqlmodel import Field, Session, SQLModel, create_engine, select, table, true
 
+
 class TaskBase(SQLModel):
     title: str = Field(index=True)
     description: str | None = Field(default=None, index=True)
