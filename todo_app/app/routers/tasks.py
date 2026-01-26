99758a096482a846b8c367189861c3004311b8f7
J. cob <jacoboosorio299589@gmail.com>
2026-01-26 18:52:38 -0500

    Adding a PUT endpoint in order to edit tasks, also adding another DTO entry for editing tasks with PUT endpoint.

--- todo_app/app/routers/tasks.py
+++ todo_app/app/routers/tasks.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 from app.database import get_session
 from app.models import Task
-from schemas.task import TaskCreate
+from schemas.task import TaskCreate, TaskUpdate
 
 router = APIRouter(prefix="/tasks", tags=["Tasks"])
 
@@ -17,6 +17,27 @@ async def create_tasks(task_data: TaskCreate, session: Session = Depends(get_ses
     db_task = Task(**task_data.model_dump())
 
     session.add(db_task)
+    session.commit()
+    session.refresh(db_task)
+
+    return db_task
+
+@router.put("/{task_id}")
+async def edit_tasks(task_id: int, new_task: TaskUpdate, session: Session = Depends(get_session)):
+    db_task = session.get(Task, task_id)
+
+    if not db_task:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if new_task.title is not None:
+        db_task.title = new_task.title
+
+    if new_task.description is not None:
+        db_task.description = new_task.description
+
+    if new_task.completed is not None:
+        db_task.completed = new_task.completed
+    
     session.commit()
     session.refresh(db_task)
 
