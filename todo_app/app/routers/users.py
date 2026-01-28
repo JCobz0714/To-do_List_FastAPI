97eddacc8c993ba872a5bc4c547a1a0da6ce396d
J. cob <jacoboosorio299589@gmail.com>
2026-01-28 17:51:04 -0500

    Creating the controller for user HTTP requests

---
+++ todo_app/app/routers/users.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session, select
+from app.database import get_session
+from app.models import User
+from schemas.user import UserCreate
+
+router = APIRouter(prefix="/users", tags=["Users"])
+
+@router.get("/{user_id}")
+async def get_users(user_id: int, session: Session = Depends(get_session)):
+    db_user = session.get(User, user_id)
+
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    return db_user
+
+@router.post("/create")
+async def create_user(user_data: UserCreate, session: Session = Depends(get_session)):
+    db_user = User(**user_data.model_dump())
+
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
