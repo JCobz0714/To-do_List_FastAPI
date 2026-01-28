a1000304dbfdd8f95faba4e0fc087a94c5b96ccd
J. cob <jacoboosorio299589@gmail.com>
2026-01-28 18:04:38 -0500

    Adding the one to many relationship between user and task inside models.py

--- todo_app/app/models.py
+++ todo_app/app/models.py
@@ -1,8 +1,11 @@
 from sqlmodel import SQLModel, Field
+from typing import Optional, List
 from .schemas import TaskBase, UserBase
 
 class Task(TaskBase, table=True):
     id: int = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user_id")
 
 class User(UserBase, table=True):
     id: int = Field(default=None, primary_key=True)
+    tasks: List["Task"] = []
