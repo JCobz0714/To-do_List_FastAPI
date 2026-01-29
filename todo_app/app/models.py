e2aa9717ad2d9f5abc0cbea70d69697f3a35039c
J. cob <jacoboosorio299589@gmail.com>
2026-01-28 19:54:38 -0500

    Fixing errors and testing endpoints. Waiting for some logic corrections in task routes

--- todo_app/app/models.py
+++ todo_app/app/models.py
@@ -1,11 +1,12 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional, List
 from .schemas import TaskBase, UserBase
 
 class Task(TaskBase, table=True):
     id: int = Field(default=None, primary_key=True)
-    user_id: int = Field(foreign_key="user_id")
+    user_id: int = Field(foreign_key="user.id")
+    user: Optional["User"] = Relationship(back_populates="tasks")
 
 class User(UserBase, table=True):
     id: int = Field(default=None, primary_key=True)
-    tasks: List["Task"] = []
+    tasks: List["Task"] = Relationship(back_populates="user")
